2dabbca59f77158d91e2cf0a3d744e10055ff073
ADoige Desktop <laozoka@gmail.com>
2019-03-14 23:27:05 -0300

    Final comments

--- NDF_main.py
+++ NDF_main.py
@@ -1,15 +1,22 @@
 """
-This proyect aims to find a number or numbers inside famous constants like pi,e, phi, etc, where the digit index of the number matchest
-the number itself, so for example the number a = 0.03451810021196314 the number 11 is on the 11th digit
+Numeral Digit Finder
+DESCRIPTION: This program finds a number (or numbers) inside known constants like pi, e, phi, etc where the digit-index of the number matches
+the number itself, so for example the number a = 0.03451810021196314 has an 11 starting at the 11th digit
 
-The massive constants were made using the program y-cruncher (Copyright 2008-2020 by Alexander J. Yee)
+AUTHOR: Agustin Marcelo Dominguez // A.Doige [March] [2019]
 
+LICENSE: MIT License (c) [2019] Agustin Marcelo Dominguez
+		Permission, free of charge, to any person to deal in the Software without restriction, 
+		including without limitation the rightsto use, copy, modify, merge, publish, distribute, and/or sublicense with the condition that
+		the software is proved "as is", without warranty of any kind, express or implied.
+
+NOTE: The massive constants were generated using the program y-cruncher (Copyright 2008-2020 by Alexander J. Yee) but in general they should be in a .txt file
+on the same folder as this file. It only reads the decimals, so a period (.) should be before the digits that are to be checked
 """
-
-constant = "pi"
 import datetime
 print("Running...")
 
+constant = "pi"
 with open("constant.txt",'r') as f:
 	with open("results.txt",'w') as g:
 		g.write("NDF Started about constant {} at {}\n".format(constant,datetime.datetime.utcnow()))
