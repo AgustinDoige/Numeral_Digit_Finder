d2bc84c7cc91e42fc3aaf5ad29340d525aa40d66
ADoige Desktop <laozoka@gmail.com>
2019-03-14 22:40:18 -0300

    Made a better version. Still slow tho

--- NDF_main.py
+++ NDF_main.py
@@ -13,25 +13,24 @@ print("Running...")
 with open("constant.txt",'r') as f:
 	with open("results.txt",'w') as g:
 		g.write("NDF Started about constant {} at {}".format(constant,datetime.datetime.utcnow()))
-		d = f.read(1)
-		counter = 0
-		while (d != '.'):  # This skips the numbers before the point and the point itself, so to get directly to the digits
-			counter += 1
-			d = f.read(1)
+		candidate = f.read(1)
+		while (candidate != '.'):  # This skips the numbers before the point and the point itself, so to get directly to the digits
+			candidate = f.read(1)
 		testing = 1
-		print("Count",counter)
-		while (d != ''):
-			try:
-				f.seek(testing+counter)
-				d = f.read(len(str(testing)))
-			except ValueError:
-				pass
-			if d == str(testing):
-				print("Found!:",d)
-				g.write("Result found: {}        - {}".format(d,datetime.datetime.utcnow()))
+		order = 1 # Amount of digits
+		current_limit = 10**order
+		while candidate != '':
+			if testing == current_limit:
+				order += 1
+				current_limit = 10**order
+				candidate = candidate[1:] + f.read(2)
+			else:
+				candidate = candidate[1:] + f.read(1)
+
+			if str(testing) == candidate:
+				print("Found!",testing)
+
 			testing += 1
-			# try:
-				# if (testing%100000 == 0):
-					# print("Testing",testing)
-			# except Exception:
-				# pass
+
+			if testing%1000000 == 0:
+				print("Testing {} millones.".format(int(testing/1000000)))
